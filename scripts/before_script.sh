da17b831f284257e6dd70cbcab1851b63807ae24
nsshewale <150108503+nsshewale@users.noreply.github.com>
2023-12-22 16:29:04 +0530

    Update before_script.sh

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -4,10 +4,10 @@ echo "** nitesh-api-prod process status **" >> "/tmp/nitesh-api-prod_deploy_logs
 runuser -l root -c 'sudo pm2 status' | grep -wo sample
 if [  $? -ne 0 ];
 then
-   echo "############################## pm2 not running #################################" >> /tmp/(directory name)-api-prod_deploy_logs
+   echo "############################## pm2 not running #################################" >> /tmp/nitesh-api-prod_deploy_logs
 else
-   echo "############################## pm2 already running Deleting ####################" >> /tmp/(directory name)-api-prod_deploy_logs
-    runuser -l ubuntu -c 'sudo pm2 delete sample'
+   echo "############################## pm2 already running Deleting ####################" >> /tmp/nitesh-api-prod_deploy_logs
+    runuser -l root -c 'sudo pm2 delete sample'
 fi
 
 rm -rf /home/ubuntu/nitesh
